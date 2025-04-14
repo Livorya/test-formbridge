b40c64da53b3ba2f6673a7c51ee3eaf2a132f458
Malin W <malin.wiren@hotmail.com>
2025-04-14 12:11:50 +0200

    added e2e-test for login

---
+++ E2ETesting-main/Features/ScenarioLogin.feature
@@ -0,0 +1,8 @@
+﻿Feature: Login
+
+    Scenario: Login as admin
+        Given I am on the login page
+        When I enter "admin1" as the email
+        And I enter "a" as the password
+        And I click the sign in button
+        Then I should redirect to the admin-dashboard
---
+++ E2ETesting-main/Steps/ScenarioSteps.cs
@@ -0,0 +1,62 @@
+﻿namespace E2ETesting.Steps;
+
+using Microsoft.Playwright;
+using TechTalk.SpecFlow;
+using Xunit;
+
+[Binding]
+public class ScenarioSteps
+{
+    private IPlaywright _playwright;
+    private IBrowser _browser;
+    private IBrowserContext _context;
+    private IPage _page;
+
+    [BeforeScenario]
+    public async Task Setup()
+    {
+        _playwright = await Playwright.CreateAsync();
+        _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = false, SlowMo = 200 });
+        _context = await _browser.NewContextAsync();
+        _page = await _context.NewPageAsync();
+    }
+
+    [AfterScenario]
+    public async Task Teardown()
+    {
+        await _browser.CloseAsync();
+        _playwright.Dispose();
+    }
+
+    [Given(@"I am on the login page")]
+    public async Task GivenIAmOnTheLoginPage()
+    {
+        await _page.GotoAsync("http://localhost:5173/login");
+    }
+
+    [When(@"I enter ""(.*)"" as the email")]
+    public async Task WhenIEnterAsTheEmail(string email)
+    {
+        await _page.FillAsync("input[name='email']", email);
+    }
+
+    [When(@"I enter ""(.*)"" as the password")]
+    public async Task WhenIEnterAsThePassword(string password)
+    {
+        await _page.FillAsync("input[name='password']", password);
+    }
+    
+    [When(@"I click the sign in button")]
+    public async Task WhenIClickTheSignInButton()
+    {
+        await _page.ClickAsync("button");
+    }
+
+    [Then(@"I should redirect to the admin-dashboard")]
+    public async Task ThenIShouldRedirectToTheAdminDashboard()
+    {
+        await _page.WaitForURLAsync("http://localhost:5173/admin");
+        var title = await _page.InnerTextAsync("h2");
+        Assert.Equal("Admin Dashboard", title);
+    }
+}
