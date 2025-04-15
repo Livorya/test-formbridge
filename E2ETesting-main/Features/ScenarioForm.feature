6257fd26077caf6a0608e15c8f8dae8af0aae624
Malin W <malin.wiren@hotmail.com>
2025-04-15 12:17:09 +0200

    added scenarios for navigating to form page

---
+++ E2ETesting-main/Features/ScenarioForm.feature
@@ -0,0 +1,12 @@
+﻿Feature: Fill out form as a customer
+
+    Scenario: Navigate to company selector page
+        Given I am on the startpage
+        When I click the "[Demo] Customer Forms" link
+        Then I am redirected to the company selector page
+
+    Scenario: Select company and navigate to form page
+        Given I am on the company selector page
+        When I select the "Komplett" option in the dropdown
+        And I click the "pick" button
+        Then I am redirected to the form page
--- E2ETesting-main/Steps/ScenarioSteps.cs
+++ E2ETesting-main/Steps/ScenarioSteps.cs
@@ -16,7 +16,7 @@ public class ScenarioSteps
     public async Task Setup()
     {
         _playwright = await Playwright.CreateAsync();
-        _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = false, SlowMo = 200 });
+        _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = false, SlowMo = 400 });
         _context = await _browser.NewContextAsync();
         _page = await _context.NewPageAsync();
     }
@@ -46,8 +46,8 @@ public class ScenarioSteps
         await _page.FillAsync("input[name='password']", password);
     }
     
-    [When(@"I click the sign in button")]
-    public async Task WhenIClickTheSignInButton()
+    [When(@"I click the button")]
+    public async Task WhenIClickTheButton()
     {
         await _page.ClickAsync("button");
     }
@@ -58,5 +58,85 @@ public class ScenarioSteps
         await _page.WaitForURLAsync("http://localhost:5173/admin");
         var title = await _page.InnerTextAsync("h2");
         Assert.Equal("Admin Dashboard", title);
+    }
+
+    [Then(@"I should redirect to the support-dashboard")]
+    public async Task ThenIShouldRedirectToTheSupportDashboard()
+    {
+        await _page.WaitForURLAsync("http://localhost:5173/support");
+        var title = await _page.InnerTextAsync("h2");
+        Assert.Equal("Support Dashboard", title);
+    }
+
+    [Given(@"I am on the startpage")]
+    public async Task GivenIAmOnTheStartpage()
+    {
+        await _page.GotoAsync("http://localhost:5173");
+    }
+
+    [When(@"I click the ""(.*)"" link")]
+    public async Task WhenIClickTheLink(string link)
+    {
+        await _page.ClickAsync($"Span:Text(\"{link}\")");
+    }
+
+    [Then(@"I am redirected to the company selector page")]
+    public async Task ThenIAmRedirectedToTheCompanySelectorPage()
+    {
+        await _page.WaitForURLAsync("http://localhost:5173/customer");
+        var title = await _page.InnerTextAsync("h1");
+        Assert.Equal("Choose Company", title);
+    }
+
+    [Given(@"I am on the company selector page")]
+    public async Task GivenIAmOnTheCompanySelectorPage()
+    {
+        await _page.GotoAsync("http://localhost:5173/customer");
+    }
+
+    [When(@"I select the ""(.*)"" option in the dropdown")]
+    public async Task WhenISelectTheOptionInTheDropdown(string selectedOption)
+    {
+        //await _page.FillAsync("option:has-text(\"{selectedOption}\")", selectedOption);
+        //await _page.SelectOptionAsync(".company-select", new SelectOptionValue { Label = selectedOption });
+        //await _page.SelectOptionAsync(".company-select", new[] { "2" });
+        
+        var options = _page.Locator("select.company-select option");
+        int count = await options.CountAsync();
+
+        string? valueToSelect = null;
+
+        for (int i = 0; i < count; i++)
+        {
+            var text = await options.Nth(i).InnerTextAsync();
+            if (text.Contains(selectedOption))
+            {
+                valueToSelect = await options.Nth(i).GetAttributeAsync("value");
+                break;
+            }
+        }
+
+        if (valueToSelect != null)
+        {
+            await _page.SelectOptionAsync(".company-select", new[] { valueToSelect });
+        }
+        else
+        {
+            Console.WriteLine($"Option containing text '{selectedOption}' not found.");
+        }
+    }
+
+    [When(@"I click the ""(.*)"" button")]
+    public async Task WhenIClickTheButton(string button)
+    {
+        await _page.ClickAsync($"button:Text(\"{button}\")");
+    }
+
+    [Then(@"I am redirected to the form page")]
+    public async Task ThenIAmRedirectedToTheFormPage()
+    {
+        await _page.WaitForURLAsync("http://localhost:5173/form/2");
+        var title = await _page.InnerTextAsync("div > h1");
+        Assert.Equal("Komplett form", title);
     }
 }
