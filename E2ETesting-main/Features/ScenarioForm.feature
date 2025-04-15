22fc8689390cc6db81d7621160547f6e40cfc548
Malin W <malin.wiren@hotmail.com>
2025-04-15 16:21:00 +0200

    added scenario for filling out form

--- E2ETesting-main/Features/ScenarioForm.feature
+++ E2ETesting-main/Features/ScenarioForm.feature
@@ -8,5 +8,16 @@
     Scenario: Select company and navigate to form page
         Given I am on the company selector page
         When I select the "Komplett" option in the dropdown
-        And I click the "pick" button
+        And I click the "Pick" button
         Then I am redirected to the form page
+
+    Scenario: Fill out form
+        Given I am on the form page
+        When I enter "Firstname" as the firstname
+        And I enter "Lastname" as the lastname
+        And I enter "email@email.email" as the email
+        And I check the "product" radiobutton
+        And I enter "Testing Playwright" as the message
+        And I click the "Submit" button to get an alert
+        Then I should see an alert with the text "Thank you for submitting your inquiry!"
+
--- E2ETesting-main/Steps/ScenarioSteps.cs
+++ E2ETesting-main/Steps/ScenarioSteps.cs
@@ -11,6 +11,9 @@ public class ScenarioSteps
     private IBrowser _browser;
     private IBrowserContext _context;
     private IPage _page;
+    
+    // IDialog for handling alert, saved so the logic follows Gherkin
+    private IDialog _lastDialog;
 
     [BeforeScenario]
     public async Task Setup()
@@ -37,7 +40,10 @@ public class ScenarioSteps
     [When(@"I enter ""(.*)"" as the email")]
     public async Task WhenIEnterAsTheEmail(string email)
     {
-        await _page.FillAsync("input[name='email']", email);
+        // have two different fields (Login & Form) that uses email input field with different selectors
+        // now I have one method for both (this works because they are on separate pages)
+        var emailField = _page.Locator("input[name='email'], input#email");
+        await emailField.First.FillAsync(email);
     }
 
     [When(@"I enter ""(.*)"" as the password")]
@@ -97,10 +103,6 @@ public class ScenarioSteps
     [When(@"I select the ""(.*)"" option in the dropdown")]
     public async Task WhenISelectTheOptionInTheDropdown(string selectedOption)
     {
-        //await _page.FillAsync("option:has-text(\"{selectedOption}\")", selectedOption);
-        //await _page.SelectOptionAsync(".company-select", new SelectOptionValue { Label = selectedOption });
-        //await _page.SelectOptionAsync(".company-select", new[] { "2" });
-        
         var options = _page.Locator("select.company-select option");
         int count = await options.CountAsync();
 
@@ -120,16 +122,14 @@ public class ScenarioSteps
         {
             await _page.SelectOptionAsync(".company-select", new[] { valueToSelect });
         }
-        else
-        {
-            Console.WriteLine($"Option containing text '{selectedOption}' not found.");
-        }
     }
 
     [When(@"I click the ""(.*)"" button")]
     public async Task WhenIClickTheButton(string button)
     {
-        await _page.ClickAsync($"button:Text(\"{button}\")");
+        // different buttons have different selectors and I want one method to handle all cases
+        var selectedButton = _page.Locator($"input[value=\"{button}\"], button:has-text(\"{button}\")");
+        await selectedButton.First.ClickAsync();
     }
 
     [Then(@"I am redirected to the form page")]
@@ -138,5 +138,64 @@ public class ScenarioSteps
         await _page.WaitForURLAsync("http://localhost:5173/form/2");
         var title = await _page.InnerTextAsync("div > h1");
         Assert.Equal("Komplett form", title);
+    }
+
+    [Given(@"I am on the form page")]
+    public async Task GivenIAmOnTheFormPage()
+    {
+        await _page.GotoAsync("http://localhost:5173/form/2");
+    }
+
+    [When(@"I enter ""(.*)"" as the firstname")]
+    public async Task WhenIEnterAsTheFirstname(string firstname)
+    {
+        await _page.FillAsync("input[id=firstname]", firstname);
+    }
+
+    [When(@"I enter ""(.*)"" as the lastname")]
+    public async Task WhenIEnterAsTheLastname(string lastname)
+    {
+        await _page.FillAsync("input[id=lastname]", lastname);
+    }
+
+    [When(@"I check the ""(.*)"" radiobutton")]
+    public async Task WhenICheckTheRadiobutton(string radioOption)
+    {
+        await _page.CheckAsync($"input[type='radio'][value=\"{radioOption.ToUpper()}\"]");
+    }
+
+    [When(@"I enter ""(.*)"" as the message")]
+    public async Task WhenIEnterAsTheMessage(string message)
+    {
+        await _page.FillAsync("#message", message);
+    }
+
+    [When(@"I click the ""(.*)"" button to get an alert")]
+    public async Task WhenIClickTheButtonToGetAnAlert(string button)
+    {
+        // Prepare to capture the alert
+        var tcs = new TaskCompletionSource<IDialog>();
+
+        _page.Dialog += (_, dialog) =>
+        {
+            _lastDialog = dialog;
+            tcs.SetResult(dialog);
+        };
+
+        // Click the button that triggers the alert
+        await _page.ClickAsync($"input[value=\"{button}\"]");
+
+        // Wait for the alert to appear
+        await tcs.Task;
+    }
+
+    [Then(@"I should see an alert with the text ""(.*)""")]
+    public async Task  ThenIShouldSeeAnAlertWithTheText(string expectedText)
+    {
+        Assert.NotNull(_lastDialog);
+        Assert.Contains(expectedText, _lastDialog.Message);
+
+        // Close the alert
+        await _lastDialog.AcceptAsync();
     }
 }
