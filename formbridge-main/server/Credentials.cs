42790e18ed73a4e129d9de972bf2049d916cda52
Malin W <malin.wiren@hotmail.com>
2025-04-19 13:06:39 +0200

    added Credentials.cs

---
+++ formbridge-main/server/Credentials.cs
@@ -0,0 +1,10 @@
+﻿namespace server;
+
+public static class Credentials
+{
+    public static readonly string Host = "localhost";
+    public static readonly string Port = "5432";
+    public static readonly string Username = "postgres";
+    public static readonly string Password = "psql-malin";
+    public static readonly string Database = "formbridge";
+}
