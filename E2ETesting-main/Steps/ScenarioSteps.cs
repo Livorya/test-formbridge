b253bc256093fd2f79b9d434e6bccb82a6ebcba5
Malin W <malin.wiren@hotmail.com>
2025-04-21 20:33:58 +0200

    changed SlowMo to 1000 to see if it changes anything

--- E2ETesting-main/Steps/ScenarioSteps.cs
+++ E2ETesting-main/Steps/ScenarioSteps.cs
@@ -19,7 +19,7 @@ public class ScenarioSteps
     public async Task Setup()
     {
         _playwright = await Playwright.CreateAsync();
-        _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = true, SlowMo = 0 });
+        _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = true, SlowMo = 1000 });
         _context = await _browser.NewContextAsync();
         _page = await _context.NewPageAsync();
     }
