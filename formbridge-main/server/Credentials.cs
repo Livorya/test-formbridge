29fbe364e874a29a6c1fa4c85c2b3e19aa5565b8
Malin W <malin.wiren@hotmail.com>
2025-04-21 17:47:20 +0200

    changed to cloud database

--- formbridge-main/server/Credentials.cs
+++ formbridge-main/server/Credentials.cs
@@ -2,9 +2,9 @@
 
 public static class Credentials
 {
-    public static readonly string Host = "localhost";
-    public static readonly string Port = "5432";
+    public static readonly string Host = "217.76.56.135";
+    public static readonly string Port = "5433";
     public static readonly string Username = "postgres";
-    public static readonly string Password = "psql-malin";
-    public static readonly string Database = "formbridge";
+    public static readonly string Password = "ConfusedSandwichReading26";
+    public static readonly string Database = "postgres";
 }
